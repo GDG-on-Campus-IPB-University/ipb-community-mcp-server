f1507c39d8d329c5c3281306c62b390be1213bf3
ekkymulia <ekky.mulialasardi@gmail.com>
2025-07-27 17:58:16 +0700

    v1.1.3

---
+++ build/data/gdgoc.js
@@ -0,0 +1,67 @@
+let GDGOCDATA = [
+    {
+        name: "WHATISGDGOC",
+        data: "GDG on Campus (formerly known as GDSC) is a university-based community where students can learn, innovate, and connect with the tech industry, all while being part of a vibrant and supportive environment. It's a platform for students interested in Google technologies to grow their development skills, build solutions, and make a real impact. "
+    },
+    {
+        name: "WHATISGDGOCIPB",
+        data: "GDG on Campus (GDGOC) IPB University is a student-led community at Institut Pertanian Bogor (IPB) University focused on empowering students in technology through resources, mentorship, and project-based learning, integrated within the global Google Developer Groups (GDG) ecosystem."
+    },
+    {
+        name: "PREVIOUSNAMEGDGOCIPB",
+        data: "Before 2024, GDGOC IPB University was known as Google Developer Student Clubs (GDSC) IPB University."
+    },
+    {
+        name: "FOUNDINGDATEGDSCIPB",
+        data: "The Google Developer Student Clubs (GDSC) at IPB University was established and officially launched on March 13, 2018."
+    },
+    {
+        name: "FIRSTLEADGDSCIPB",
+        data: "The first Lead of GDSC IPB University was Vito Rizki Imanda from the Department of Computer Science, FMIPA IPB."
+    },
+    {
+        name: "GDSCIPBVACUUM",
+        data: "After Vito Rizki Imanda's initial leadership, GDSC IPB University entered a dormant phase from 2019 to 2023 due to a lack of successor."
+    },
+    {
+        name: "REVIVALLEADERGDSCIPB",
+        data: "The community was revived by Muhammad Rizki Agus Salim from Software Engineering Technology, Vocational School."
+    },
+    {
+        name: "ACHIEVEMENTSREVIVALGDSCIPB",
+        data: "Under the revived leadership, GDSC IPB University held over four events, including a collaboration with HIMAVO Micro-IT for the national ITFEST 2024, and grew its membership to 320 on the GDG platform."
+    },
+    {
+        name: "VISIONGDGOCIPB2024_2025",
+        data: "The vision for GDGOC IPB University for 2024-2025 includes a focus on education and learning, validating skills, fostering collaboration with other GDGOC chapters, and building a high-quality, inclusive, exclusive, and sustainable community, aiming to be a platform for technology collaboration for students inside and outside IPB."
+    },
+    {
+        name: "MISSIONGDGOCIPB2024_2025",
+        data: "The mission statement for GDGOC IPB University for 2024-2025 is 'Progressing Beyond the Capacity.'"
+    },
+    {
+        name: "CURRENTLEADGDGOCIPB",
+        data: "The current Lead for GDGOC IPB University for the 2024-2025 period is Ekky Mulia Lasardi from the Software Engineering Technology program at the Vocational School of IPB University."
+    },
+    {
+        name: "LEADBACKGROUNDGDGOCIPB",
+        data: "Ekky Mulia Lasardi has 5 years of coding experience, has won several competitions, was a finalist in Gemastik 2024, and served as the Chief Organizer for the GDSC IPB x HIMAVO Micro-IT National Competition."
+    },
+    {
+        name: "MEMBERSHIPTYPESGDGOCIPB",
+        data: "GDGOC IPB University has two membership types: Anggota Tetap/Golden Member (committed members with full access to program facilities) and Anggota Terdaftar (officially registered members with limited access)."
+    },
+    {
+        name: "HOWTOJOINGDGOCIPB",
+        data: "To join GDGOC IPB University, visit [https://gdg.community.dev/gdg-on-campus-ipb-university-bogor-indonesia/](https://gdg.community.dev/gdg-on-campus-ipb-university-bogor-indonesia/) and click 'Join' to become a Registered Member. or Chat this AI via Active Member Registration"
+    },
+    {
+        name: "KEYCHANGESGDSCTOGDGOCIPB",
+        data: "The transition from GDSC to GDGOC at IPB University in 2024 brought a stronger focus on education and learning, a change in the Lead's term rule (theoretically unlimited but with yearly democratic elections), and the local GDG on Campus and Lead having full decision on Lead selection."
+    },
+    {
+        name: "PLANNEDPROGRAMSGDGOCIPB",
+        data: "Planned programs for 2024-2025 include Knowledge Sharing with Learning Paths in Web Development, Mobile Development, Machine Learning with AI, Web 3 & Crypto Trading, Product Development & User Experience, and Community Building; Monthly Catch-up and Gathering networking sessions; GDGoC IPB University Fun Hacks competitions; and collaborations with other GDG/GDGOC chapters or campuses."
+    }
+];
+export default GDGOCDATA;
